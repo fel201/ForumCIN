f479096429723ae8c833ca529e736db484e88004
Felipe Mateus <felipemateus216@gmail.com>
2025-09-30 19:06:09 -0300

    add css file and change size of textarea in main page

--- views/submitted.js
+++ views/submitted.js
@@ -1,11 +1,3 @@
-async function displayTime() {
-    const request = await fetch('http://api.weatherapi.com/v1/current.json?key=5a0a631055c7426ab89194429251009&q=Brazil&aqi=no')
-    const data = await request.json();
-    const time = data.location.localtime;
-    document.getElementById("timezoneId").innerText = time;
-}
-setInterval(displayTime, 600000);
-
 document.getElementById("theText").addEventListener("submit", async e => {
     e.preventDefault();
     const title = e.target.titleInformation.value;
@@ -31,5 +23,4 @@ document.getElementById("theText").addEventListener("submit", async e => {
         console.log("Submission Failed", err);
     }
 });
-
 
