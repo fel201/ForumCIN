56e4b9eacd9205d7e15d24309fad781a127d9f2d
Felipe Mateus <felipemateus216@gmail.com>
2025-10-16 15:52:19 -0300

    add comment table and insertion logic

--- db/supabase connection.session.sql
+++ db/supabase connection.session.sql
@@ -3,10 +3,8 @@ CREATE TABLE comments(
     post_id     INT NOT NULL,
     content     TEXT NOT NULL,
     user_id     INT NOT NULL,
-    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT  fk_text FOREIGN KEY(post_id) REFERENCES submissions(id)
+);
 
-CONSTRAINT fk_text(
-    FOREIGN KEY(post_id),
-    REFERENCES(submissions),
-)
+SELECT * FROM comments
